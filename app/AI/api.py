f93a64faed000f84654f69b5c1f05a6e0039c78f
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-02-13 15:11:34 -0500

    add route for triage agent

--- app/AI/api.py
+++ app/AI/api.py
@@ -1,11 +1,14 @@
 import os
+from typing import Annotated
+
 import ollama
 from fastapi import APIRouter
+from fastapi.params import Body
 from starlette.responses import StreamingResponse
 
 from app.database.db import search_poi
 from app.dependencies import NeedsDb, NeedsOllama
-from app.AI.extras import instruction_prompt
+from app.AI.extras import instruction_prompt, triage_agent_prompt
 
 router = APIRouter()
 
@@ -32,3 +35,20 @@ async def user_query_step_1(
             yield chunk["message"]["content"]
 
     return StreamingResponse(response())
+
+@router.get("/ai/triage_agent", tags=["poi", "ai", "triage_agent"], dependencies=[NeedsOllama])
+def triage_agent(
+        user_query: str,
+        user_position: Annotated[(float, int, int), Body()]
+):
+    model = os.environ.get("AI_MODEL")
+
+    res = ollama.chat(
+        model=model,
+        messages=[
+            {"role": "system", "content": triage_agent_prompt()},
+            {"role": "user", "content": user_query}
+        ]
+    )
+
+    return res
