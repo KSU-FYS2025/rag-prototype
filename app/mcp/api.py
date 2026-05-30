d6b861aaa840187abee1e93e7dfb81a672bf4e7d
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-05-30 13:55:03 -0400

    mcp server up test

--- app/mcp/api.py
+++ app/mcp/api.py
@@ -8,3 +8,7 @@ mcp_app = mcp.http_app()
 router = APIRouter(lifespan=mcp_app.lifespan)
 
 router.mount("/mcp", mcp_app)
+
+@mcp.tool()
+def ping() -> str:
+    return "pong!"
--- app/tests/tests.py
+++ app/tests/tests.py
@@ -1,7 +1,12 @@
 from fastapi import HTTPException
+import pytest
+
+from fastmcp.client import Client
+from fastmcp.client.transports import FastMCPTransport
 
 from ..main import app
 from ..dependencies import needs_ollama
+from ..mcp.api import mcp
 from fastapi.testclient import TestClient
 
 
@@ -24,3 +29,13 @@ def test_milvus():
     response = client.get("/poi/all")
     print(response)
     assert response.json() != {}
+
+@pytest.fixture
+async def main_mcp_client():
+    async with Client(transport=mcp) as mcp_client:
+        yield mcp_client
+
+async def test_ping(main_mcp_client: Client[FastMCPTransport]):
+    res = await main_mcp_client.call_tool("ping")
+
+    assert res.content == "pong!"
