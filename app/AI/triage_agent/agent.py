f749b3326f116dec394a6baae1eb4641439b5d92
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-02 17:18:49 -0400

    update to use output_schema

--- app/AI/triage_agent/agent.py
+++ app/AI/triage_agent/agent.py
@@ -9,23 +9,7 @@ triage_agent = Agent(
     description='An agent that separates and classifies queries within a full user query',
     instruction='''You are the second agent within a chain meant to provide navigation support. Your role is to take the
     user's query and separate it into separate queries if applicable, and provide meaningful information related to the
-    query. The following is the structured json output you MUST follow in your response. Below is the non-minified
-    version. You must minify it in your response.
-    {
-        "targets": [
-            {
-                "order": <int>, position inside list
-                "intent": <str>, MUST BE ONE OF THE FOLLOWING ["navigation_query", "navigation_guidance", "conversational"]
-                "target_type": <str>, MUST BE ONE OF THE FOLLOWING ["implicit", "explicit"]. If user directly mentions
-                    place, assign to explicit, otherwise should be implicit
-                "semantics": <str>, brief information describing the place to be used for vector search
-                "source": <str>, should always be "llm"
-            },
-            ...
-        ]
-        "extraction_method": <str>, should always be "hybrid"
-    }
-    
+    query.
     example data:
     {
         "targets": [
@@ -35,7 +19,7 @@ triage_agent = Agent(
                 "target_type": "specific",
                 "semantics": "Room 3117",
                 "filter": "name LIKE '%3117%'",
-                "source": "llm"
+                "source": "LLM"
             },
             {
                 "order": 2,
@@ -43,17 +27,11 @@ triage_agent = Agent(
                 "target_type": "generic",
                 "semantics": "elevator, lift, 3rd floor",
                 "filter": "type == 'Elevator'",
-                "source": "llm"
+                "source": "LLM"
             }
         ],
-        "extraction_method": "hybrid"
+        "extraction_method": "llm"
     }''',
-    output_key="output_triage"
-)
-
-triage_validation_agent = ValidationAgent(
-    name="TriageValidationAgent",
-    description="Validates the json output of root_agent",
-    state_key="output_intent",
-    schema=TriageAgentOutput
+    output_key="output_triage",
+    output_schema=TriageAgentOutput,
 )
--- app/AI/triage_agent/schema.py
+++ app/AI/triage_agent/schema.py
@@ -1,15 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.AI.schema import TargetType, NavigationIntent
 from typing import List
 
 class QueryClassifier(BaseModel):
-    order: int
+    order: int = Field(ge=1)
     intent: NavigationIntent
     target_type: TargetType
-    semantics: str
-    source: str
+    filter: str = Field(description="A SQL-Like filter that is used to further refine the output of a vector search "
+                                    "inside pymilvus.")
+    semantics: str = Field(description="Semantic information about the query that can be used to run a vector search. "
+                                       "Include things like what the Point of Interest is, and any helpful explicit "
+                                       "information.\n"
+                                       "EX1: ROOM 2131\n"
+                                       "EX2: bathroom, third floor")
+    source: str = Field(description="Should always be LLM")
 
 class TriageAgentOutput(BaseModel):
-    targets: List[QueryClassifier]
-    extraction_method: str
+    targets: List[QueryClassifier] = Field(description="A list of query classifiers that can be used to run a vector search ")
+    extraction_method: str = Field(description="Should always be LLM")
