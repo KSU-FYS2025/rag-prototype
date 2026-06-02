432ecfa566f99cc7b51660abc13a3bcbcde9ca66
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-02 17:17:40 -0400

    properly annotate schema

--- app/AI/schema.py
+++ app/AI/schema.py
@@ -7,6 +7,7 @@ from google.adk.events import Event, EventActions
 from enum import Enum
 import json
 
+
 class ValidationAgent(BaseAgent):
     state_key: str
     schema: type[BaseModel]
@@ -50,9 +51,26 @@ class ValidationAgent(BaseAgent):
             actions=EventActions()
         )
 
+
 class NavigationIntent(str, Enum):
+    """
+    Intent of the user at each step
+    NAV_QUERY/ navigation_query - User has a query relating to navigation, but does not wish to be guided yet
+    NAV_GUIDE/ navigation_guidance - User wishes to be guided somewhere
+    CONVERSATIONAL/ conversational - User converses with the AI Agent, no navigation or additional information is needed
+    CLARIFICATION/ clarification - AI Agent requires clarification from the user
+    """
     NAV_QUERY = "navigation_query"
     NAV_GUIDE = "navigation_guidance"
     CONVERSATIONAL = "conversational"
+    CLARIFICATION = "clarification"
 
-type TargetType = Literal["implicit", "general"]
+class TargetType(str, Enum):
+    """
+    The type of the navigation target
+    IMPLICIT/ implicit - User does not directly name the room but wishes to be guided there
+    EXPLICIT/ explicit - User directly mentions the name/ number of the room they wish to be guided to
+    """
+    IMPLICIT = "implicit"
+    EXPLICIT = "explicit"
+
