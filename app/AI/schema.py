68152ea6c59c986de9a74af9b4f05a498474c736
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-11 14:35:42 -0400

    remove ValidationAgent

--- app/AI/schema.py
+++ app/AI/schema.py
@@ -7,50 +7,8 @@ from google.adk.events import Event, EventActions
 from enum import Enum
 import json
 
-
-# class ValidationAgent(BaseAgent):
-#     state_key: str
-#     schema: type[BaseModel]
-#
-#     model_config = {"arbitrary_types_allowed": True}
-#
-#     async def _run_async_impl(self, ctx: InvocationContext):
-#         raw = ctx.session.state.get(self.state_key)
-#
-#         if raw is None:
-#             raise ValueError(f"State key [{self.state_key}] is either missing or None! Check upstream models!")
-#
-#         if isinstance(raw, str):
-#             cleaned = raw.strip().removeprefix("```json").removeprefix("```").removesuffix("```").strip()
-#
-#             try:
-#                 parsed_dict = json.loads(cleaned)
-#             except json.JSONDecodeError as e:
-#                 raise ValueError(
-#                     f"[{self.name}] state['{self.state_key}'] is not valid JSON: {e}\nRaw value: {raw!r}"
-#                 )
-#         elif isinstance(raw, dict):
-#             parsed_dict = raw
-#         else:
-#             raise ValueError(
-#                 f"[{self.name}] state['{self.state_key}'] is not valid JSON\nRaw value: {raw!r}"
-#             )
-#
-#         try:
-#             validated = self.schema.model_validate(parsed_dict)
-#         except ValidationError as e:
-#             raise ValueError(
-#                 f"[{self.name}] Pydantic validation failed for '{self.state_key}':\n{e}"
-#             )
-#         # This may be unsafe, consider changing this
-#         ctx.session.state[self.state_key] = validated.model_dump_json()
-#
-#         yield Event(
-#             author=self.name,
-#             content=None,
-#             actions=EventActions()
-#         )
-
+class BaseResponse(BaseModel):
+    user_query: str = Field(description="The user's original query. MUST match the user's query exactly.")
 
 class NavigationIntent(str, Enum):
     """
