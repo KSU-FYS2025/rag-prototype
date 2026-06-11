7c601a391a31c7784098ca3d4fa174263d43931d
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-11 10:55:40 -0400

    model out data structures

--- app/AI/full_agent/response_agent/schema.py
+++ app/AI/full_agent/response_agent/schema.py
@@ -1,21 +1,49 @@
-from typing import List
+from typing import List, Literal, Annotated, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, Discriminator, Tag
 from enum import Enum
-
-class FinalResponse(BaseModel):
-    response: str
-    actions: List[Action]
-
-class Command(str, Enum):
-    """
-    The command that the Unity runner will take.
-    NAVIGATION/ navigation - Unit runner will guide the user to an id specified outside of this enum.
-    """
-    # TODO: Implement route to nearest using Unity's calculated distances
-    NAVIGATION = "navigation"
 
 class Action(BaseModel):
     order: int = Field(ge=0, description="The order of the Action")
-    cmd: Command
+
+class NavigationAction(Action):
+    cmd: Literal["navigation"] = "navigation"
     id: int = Field(description="The id of the POI the user will be guided to")
+    target_label: str = Field(description="The semantic information of the query the user will be guided to")
+
+class ResolveNearestAction(Action):
+    cmd: Literal["resolve_nearest"] = "resolve_nearest"
+    candidate_ids: List[int] = Field(description="The ids the Unity client will decide between.")
+    target_label: str = Field(description="The semantic information of the query the user will be guided to")
+
+class AnswerAction(Action):
+    cmd: Literal["answer"] = "answer"
+    text: str = Field(description="The text of the Answer")
+    source_ids: List[int] = Field(description="The poi ids the user was questioning about")
+
+class ClarificationSuggestion(BaseModel):
+    id: int = Field(description="The id of the POI you are suggesting")
+    name: str = Field(description="The name of the POI you are suggesting")
+
+class ClarifyAction(Action):
+    cmd: Literal["clarify"] = "clarify"
+    unresolved_target: str = Field(description="The semantic information of the poi the user queried")
+    reason: str = Field(description="The reason the agent was not able to resolve this query.")
+    suggestions: List[ClarificationSuggestion] = Field(description="Suggestions for POIs that exist")
+    prompt: str = Field(description="The prompt that will be shown to the user. Must include all the suggestions in "
+                                    "suggestions.")
+
+Command = Annotated[
+    Union[
+        Annotated[NavigationAction, Tag("navigation")],
+        Annotated[ResolveNearestAction, Tag("resolve_nearest")],
+        Annotated[AnswerAction, Tag("answer")],
+        Annotated[ClarifyAction, Tag("clarify")]
+    ],
+    Discriminator("cmd")
+]
+
+class FinalResponse(BaseModel):
+    response: str = Field(description="The final response from the Unity runner that will be spoken to the user.")
+    actions: List[Command] = Field(description="The list of actions the Unity runner will take. This should be in the "
+                                               "order the Unity runner takes them.")
