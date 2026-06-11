97410c520597e3d85b27cddf7c1ed0687543bf4f
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-11 12:25:51 -0400

    connect conversation agent to full_workflow
    If intent is conversational, it routes it to the conversation agent. Otherwise, do the full navigation workflow

--- app/AI/full_agent/agent.py
+++ app/AI/full_agent/agent.py
@@ -1,12 +1,39 @@
+from google.adk.workflow import node
+
+from app.AI.full_agent.conversation_agent.agent import conversation_agent
 from app.AI.full_agent.root_agent.agent import root_agent
+from app.AI.full_agent.root_agent.schema import RootOutput
 from app.AI.full_agent.triage_agent.agent import triage_agent
 from app.AI.full_agent.search_agent.agent import search_workflow
 
-from google.adk import Workflow
+from google.adk import Workflow, Event
+
+from app.AI.schema import NavigationIntent
+
+navigation_workflow = Workflow(
+    name='navigation_workflow',
+    edges=[
+        ("START", triage_agent, search_workflow),
+    ]
+)
+
+@node(name="navigation_router", rerun_on_resume=True)
+def navigation_router(node_input: RootOutput):
+    if node_input.intent == NavigationIntent.CONVERSATIONAL:
+        return Event(route="conversational")
+    else:
+        return Event(route="navigation")
+
 
 full_workflow = Workflow(
     name="full_workflow",
     edges=[
-        ("START", root_agent, triage_agent, search_workflow),
+        ("START", root_agent, navigation_router),
+        (navigation_router,
+             {
+                 "conversational": conversation_agent,
+                 "navigation": navigation_workflow
+             }
+         )
     ]
 )
