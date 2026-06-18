9bcff3c8b3a3c42b8d9a708468c365b1fc84359c
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-18 17:13:44 -0400

    Revert "response_agent can recall memory"
    This reverts commit cf0a19a7a9e9cd6d08fd1bfb10c53e9c45b397a4.

--- app/AI/full_agent/response_agent/agent.py
+++ app/AI/full_agent/response_agent/agent.py
@@ -1,5 +1,4 @@
 from google.adk.agents.llm_agent import Agent
-from google.adk.tools import load_memory
 
 from app.AI.full_agent.response_agent.schema import ResponseAgentOutput
 from app.AI.full_agent.search_agent.schema import SearchOutput
@@ -11,9 +10,7 @@ response_agent = Agent(
     instruction='''You are the last agent in a chain meant to provide navigational assistance. Your role is to take a
     list of POIs returned by the last agent and use it to synthesize a list of actions for the Unity client to take. For
     each object inside the validations tag, you must assign one action to it. Be sure to always take user preferences 
-    into account if it is necessary. In cases where you need user preferences, you may use the load_memory tool to find
-    it in previous conversations.''',
+    into account if it is necessary.''',
     input_schema=SearchOutput,
     output_schema=ResponseAgentOutput,
-    tools=[load_memory],
 )
