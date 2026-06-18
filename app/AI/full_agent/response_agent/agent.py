65945ea56fef60c33778a474eca0551155b7026b
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-18 17:12:55 -0400

    start updated agent layout

--- app/AI/full_agent/response_agent/agent.py
+++ app/AI/full_agent/response_agent/agent.py
@@ -12,7 +12,7 @@ response_agent = Agent(
     list of POIs returned by the last agent and use it to synthesize a list of actions for the Unity client to take. For
     each object inside the validations tag, you must assign one action to it. Be sure to always take user preferences 
     into account if it is necessary. In cases where you need user preferences, you may use the load_memory tool to find
-    it in previous conversations.''',
+    any preferences need from previous conversations.''',
     input_schema=SearchOutput,
     output_schema=ResponseAgentOutput,
     tools=[load_memory],
--- app/AI/full_agent/response_agent/schema.py
+++ app/AI/full_agent/response_agent/schema.py
@@ -71,6 +71,6 @@ class ClarifyAction(Action):
 Command = Union[NavigationAction, ResolveNearestAction, AnswerAction, ClarifyAction]
 
 class ResponseAgentOutput(BaseResponse):
-    response: str = Field(description="The final response from the Unity runner that will be spoken to the user.")
+    # response: str = Field(description="The final response from the Unity runner that will be spoken to the user.")
     actions: List[Command] = Field(description="The list of actions the Unity runner will take. This should be in the "
                                                "order the Unity runner takes them.")
