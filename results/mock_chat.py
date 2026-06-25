1d14cdff632ae816cbdaf9cc02d74488823b5dfa
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-06-25 15:18:31 -0400

    enable thinking

--- results/mock_chat.py
+++ results/mock_chat.py
@@ -4,7 +4,7 @@ import requests
 adk_data = {
     "app_name": "full_agent",
     "user_id": "user",
-    "evalset_name": "full_agent_eval_set_large_9_enabled",
+    "evalset_name": "full_agent_eval_set_large_10_enabled",
 }
 
 adk_url = "http://10.96.50.180:8080"
