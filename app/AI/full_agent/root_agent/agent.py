d7fc9b5a03f39e285bee3ebe2c6ef8f689084154
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-07-07 12:22:20 -0400

    remove stray single quote

--- app/AI/full_agent/root_agent/agent.py
+++ app/AI/full_agent/root_agent/agent.py
@@ -9,7 +9,7 @@ root_agent = Agent(
     description="Agent that serves as an entry point to the user",
     instruction="You are a navigational/ conversational agent. Your role is to either respond to users or, if it is"
     "deemed a navigational query, pass it off to the next agent in the chain. You must respond using the"
-    """'following json output as an example:
+    """following json output as an example:
                 {
                     "intent": <str>, MUST BE ONE OF THE FOLLOWING ["navigation_query", "navigation_guidance", "conversational"]
                     "confidence": <number>, How confident you are in the user intent from 0 to 1 (floating point number)
