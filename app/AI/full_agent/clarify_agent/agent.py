9b6a6cb55c7e6fdb4140cef84b8bd99449387a86
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-07-10 14:05:32 -0400

    assert Command as type

--- app/AI/full_agent/clarify_agent/agent.py
+++ app/AI/full_agent/clarify_agent/agent.py
@@ -2,6 +2,7 @@ from app.AI.full_agent.actions_agent.schema import Command
 from app.AI.full_agent.agent_builder import AgentBuilder
 from app.AI.full_agent.clarify_agent.schema import ClarifyInput
 
+assert isinstance(Command, type)
 clarify_agent = AgentBuilder(
     name="clarify_agent",
     description="An agent whose purpose is to resolve clarification actions.",
