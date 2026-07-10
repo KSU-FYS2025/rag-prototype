a1f581c4cff43835dea3822ae51c41822d37babb
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2026-07-10 14:15:26 -0400

    remove unused files

--- app/AI/full_agent/defaults.py
+++
@@ -1,8 +0,0 @@
-from google.adk.planners import BuiltInPlanner
-from google.genai import types
-
-planner_defaults = BuiltInPlanner(
-    thinking_config=types.ThinkingConfig(
-        include_thoughts=False,
-    )
-)
--- app/AI/full_agent/instructions.py
+++
@@ -1,9 +0,0 @@
-from google.adk.agents.readonly_context import ReadonlyContext
-
-
-def shared_instruction_provider(context: ReadonlyContext) -> str:
-    agent_name = context.agent_name
-    with open(f"instructions/{agent_name}.md") as f:
-        instructions = f.readlines()
-
-    return "\n".join(instructions)
